d098a95d3fdaaf098ad57adb37cd91950eecf92a
manasvipinnamaneni <manasvipinnamaneni@gmail.com>
2026-09-22 19:50:54 +0530

    Time: 31 ms (55.21%), Space: 48.7 MB (16.65%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -3,16 +3,12 @@ class Solution {
         int n = candyType.length;
         HashSet<Integer> set = new HashSet<>();
         int k = 0;
-        for(int i = 0; i < n; i++) {
-            if(!set.contains(candyType[i])) {
-                set.add(candyType[i]);
-                candyType[k] = candyType[i];
-                k++;
-            }
+        for(int num : candyType) {
+            set.add(num);
         }
-        if(k > n/2) {
+        if(set.size() > n/2) {
             return n/2;
         }
-        return k;
+        return set.size();
     }
 }
